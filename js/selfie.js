f1ce69f1afa17579c9cdf20b1703ee8bd453a52f
sschlink-pratt <sschlink@pratt.edu>
2026-05-07 20:40:51 -0400

    Simplifies selfie.js

--- js/selfie.js
+++ js/selfie.js
@@ -1,23 +1,19 @@
-// Hide the "film" element where the selfie image will eventually
-// appear
-var filmElement = document.getElementById("selfie-upload-by-user");
-filmElement.style.display = "none";
-
-// When a user uploads an image.... 
-function onSelfieUpload(event) {
-    // Get the file input element that triggered the event
-    var fileInput = event.target;
+// This function is called when a user uploads an image to 
+// appear in the helmet.
+function onSelfieUpload() {
+    let fileInput = document.getElementById("selfie");
     // Technically, it's possible to upload multiple files.
     // But we just want 1 image, so we'll just take the first 
     // file.
-    var uploadedFile = fileInput.files[0];
-    
-    // Check if a file was actually selected
+    let uploadedFile = fileInput.files[0];
+    let filmElement = document.getElementById("selfie-upload-by-user");
+
+    // Do basic check of whether the file exists
     if (uploadedFile) {
         // We now have to create this thing called an ObjectURL 
         // for the selected file 
         // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL_static
-        var uploadedFileURL = URL.createObjectURL(uploadedFile);
+        let uploadedFileURL = URL.createObjectURL(uploadedFile);
         
         // Which we can then set to be the source of filmElement
         filmElement.src = uploadedFileURL;
